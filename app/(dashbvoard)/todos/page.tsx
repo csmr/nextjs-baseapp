49aa30aa46eb8a3f56831f8c5f93693c5adf761c
Casimir Pohjanraito <casimir.pohjanraito@vincit.fi>
2024-03-21 13:34:03 +0200

    add todo db logic

--- app/(dashbvoard)/todos/page.tsx
+++ app/(dashbvoard)/todos/page.tsx
@@ -1,5 +1,21 @@
-const DashboardPage = () => {
-    return <div>Dashboard todos</div>
+import TodoList from '@/components/TodoList'
+import db from '@/utils/db'
+
+const getData = async () => {
+    // note lowercase todo for db - schema Todos 
+    const todos = await db.todo.findMany({})
+    // Where is this log going to be rendered?
+    console.log("todos:", todos)
+    return todos
 }
 
-export default DashboardPage
+const TodosPage = async () => {
+    const todos = await getData()
+    return ( 
+        <div>
+            <TodoList todos={todos} />
+        </div>
+    )
+}
+
+export default TodosPage
